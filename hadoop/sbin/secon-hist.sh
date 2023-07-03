ef99cfe58b04abbd851e44a7aef4b8d440d6421c
blacktrigger97 <blacktrigger97@gmail.com>
2023-07-03 11:07:49 +0530

    Mariadb Setup 13

--- hadoop/sbin/secon-hist.sh
+++ hadoop/sbin/secon-hist.sh
@@ -44,7 +44,7 @@ ssh-keygen -t rsa -f /etc/ssh/ssh_host_ed25519_key -N ''
 /usr/sbin/crond
 
 USR=`echo ${DOCKER_DIR} | rev | cut -d "/" -f 2 | rev`
-/usr/libexec/mariadbd --user=$USR --datadir=${DOCKER_DIR}${MARIADB_DIR} --pid-file=${DOCKER_DIR}${MARIADB_DIR} --log-error=${DOCKER_DIR}${MARIADB_DIR}/logs &
+/usr/libexec/mariadbd --user=$USR --datadir=${DOCKER_DIR}${MARIADB_DIR} --pid-file=${DOCKER_DIR}${MARIADB_DIR} &
 
 ## @description  usage info
 ## @audience     private
