a55edaee9b28d736430d3d291db30b723da2ed68
blacktrigger97 <blacktrigger97@gmail.com>
2023-07-03 13:53:52 +0530

    Mariadb Setup 27

--- mariadb/init/init.sql
+++ mariadb/init/init.sql
@@ -1,3 +1,3 @@
-CREATE USER 'hive'@'%' IDENTIFIED BY 'hive' password expire never;
+CREATE USER 'hive'@'%' IDENTIFIED BY 'hive' password expire never with grant option;
 GRANT ALL ON *.* TO 'hive'@'%';
 flush privileges;
