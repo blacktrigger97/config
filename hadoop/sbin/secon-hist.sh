97de4b4a6d3a616d9b9c584a11640079095e8e12
blacktrigger97 <blacktrigger97@gmail.com>
2023-07-03 09:35:01 +0530

    Mariadb Setup 5

--- hadoop/sbin/secon-hist.sh
+++ hadoop/sbin/secon-hist.sh
@@ -75,7 +75,7 @@ fi
 
 USR=`echo ${DOCKER_DIR} | rev | cut -d "/" -f 2 | rev`
 
-/usr/libexec/mariadbd --user=$USR &
+/usr/libexec/mariadbd --user=$USR --datadir=${DOCKER_DIR}${MARIADB_DIR} &
 
 # Hive user & its priviledge initialization
 mysql < ${DOCKER_DIR}init.sql
