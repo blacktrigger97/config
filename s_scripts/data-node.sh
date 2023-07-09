1ed0093d92c1e9c752c5b027b9fc5759e4160615
blacktrigger97 <blacktrigger97@gmail.com>
2023-07-09 18:26:03 +0530

    Spark Setup 11

--- s_scripts/data-node.sh
+++ s_scripts/data-node.sh
@@ -69,7 +69,7 @@ if [ $? -eq 0 ]; then
 	"${HADOOP_HDFS_HOME}/bin/hdfs" dfs -mkdir -p /tmp
 	"${HADOOP_HDFS_HOME}/bin/hdfs" dfs -mkdir -p /root/hive/warehouse
 	"${HADOOP_HDFS_HOME}/bin/hdfs" dfs -mkdir -p /root/spark/logs
-	"${HADOOP_HDFS_HOME}/bin/hdfs" dfs -chmod -R 777 /
+	"${HADOOP_HDFS_HOME}/bin/hdfs" dfs -chmod -R 777 /root
 fi
 
 "${HADOOP_HDFS_HOME}/bin/yarn"  nodemanager &
