56d6352340cc9737fa408aec7e7984a717116bf8
blacktrigger97 <blacktrigger97@gmail.com>
2023-07-12 22:42:17 +0530

    Mysql Setup D1 14

--- s_scripts/secon-hist.sh
+++ s_scripts/secon-hist.sh
@@ -78,7 +78,7 @@ fi
 
 sleep 120
 
-if [[ ! -z "`mysql -u hive -h mariadb -phive -qfsBe "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME='hive_metastore'" 2>&1`" ]];
+if [[ ! -z "`mysql -u hive -h mysql -phive -qfsBe "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME='hive_metastore'" 2>&1`" ]];
 then
   echo "DATABASE ALREADY EXISTS"
 else
