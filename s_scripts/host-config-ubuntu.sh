427631a715eaff714250cbeb32044b98a8b4cbd3
blacktrigger97 <blacktrigger97@gmail.com>
2023-07-16 14:05:42 +0530

    Upgrade D1 3

--- s_scripts/host-config-ubuntu.sh
+++ s_scripts/host-config-ubuntu.sh
@@ -20,7 +20,13 @@ do
 	echo $i
 	if [[ "$i" == "$(hostname)" ]]; then
 		echo "Updating shared hosts file"
-		sed -i -E "s/.*$(hostname)/$ip_addr\t$(hostname)/g" ${DOCKER_DIR}hosts/hosts
+		IFS=$'\n' read -r -d '' -a sh_host_fl_ip < <(cat ${DOCKER_DIR}hosts/hosts | grep $i | cut -d $'\t' -f1 | awk '{print $1;}')
+		for j in ${sh_host_fl_ip[@]}
+		do
+			if [[ "$ip_addr" != "$j" ]]; then
+				sed -i -E "s/.*$(hostname)/$ip_addr\t$(hostname)/g" ${DOCKER_DIR}hosts/hosts
+			fi
+		done
 	else
 		if [[ ! " ${host_addr[*]} " =~ " $i " ]]; then
 			echo "Adding other node to hosts file"
@@ -38,6 +44,7 @@ done
 
 if [[ ! " ${hdc_addr[*]} " =~ " $(hostname) " ]]; then
     	# whatever you want to do when array doesn't contain value
-	echo "$ip_addr    $(hostname)" >> ${DOCKER_DIR}hosts/hosts
+	echo -e "$ip_addr\t$(hostname)" >> ${DOCKER_DIR}hosts/hosts
 fi
 
+awk -i inplace '!seen[$0]++' ${DOCKER_DIR}hosts/hosts
