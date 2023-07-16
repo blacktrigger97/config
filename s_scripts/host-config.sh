b313c5abc13782041475d132d3f3ea3af73ecd8a
blacktrigger97 <blacktrigger97@gmail.com>
2023-07-16 14:46:31 +0530

    upgrade D1 5

--- s_scripts/host-config.sh
+++ s_scripts/host-config.sh
@@ -8,30 +8,35 @@ ip_addr=`ifconfig eth0 | grep 'inet ' | awk '{print $2}'`
 cat /etc/hosts | grep -E $(hostname)*
 if [ $? -eq 1 ]; then
 	echo "Adding host eth0 IP in hosts file"
-	echo "$ip_addr    $(hostname)" >> /etc/hosts
+	echo -e "${ip_addr}\t$(hostname)" >> /etc/hosts
 fi
 
 IFS=$'\n' read -r -d '' -a host_addr < <(cat /etc/hosts | awk '{print $2}')
 IFS=$'\n' read -r -d '' -a hdc_addr < <(cat ${DOCKER_DIR}hosts/hosts | awk '{print $2}')
+echo ${host_addr[@]}
+echo ''
+echo ${hdc_addr[@]}
 for i in ${hdc_addr[@]}
 do
 	echo $i
 	if [[ "$i" == "$(hostname)" ]]; then
 		echo "Updating shared hosts file"
 		IFS=$'\n' read -r -d '' -a sh_host_fl_ip < <(cat ${DOCKER_DIR}hosts/hosts | grep $i | cut -d $'\t' -f1 | awk '{print $1;}')
+		echo ${sh_host_fl_ip[@]}
 		for j in ${sh_host_fl_ip[@]}
 		do
 			if [[ "$ip_addr" != "$j" ]]; then
+				echo "Writing in Shared hosts file"
 				sed -i -E "s/.*$(hostname)/$ip_addr\t$(hostname)/g" ${DOCKER_DIR}hosts/hosts
 			fi
 		done
 	else
 		if [[ ! " ${host_addr[*]} " =~ " $i " ]]; then
 			echo "Adding other node to hosts file"
-			cat ${DOCKER_DIR}hosts/hosts | grep -E $i >> /etc/hosts
+			cat ${DOCKER_DIR}hosts/hosts | grep $i >> /etc/hosts
 		else
 			echo "Updating hosts file"
-			upd_addr=`cat ${DOCKER_DIR}hosts/hosts | grep -E $i | awk '{print $1}'`
+			upd_addr=`cat ${DOCKER_DIR}hosts/hosts | grep $i | awk '{print $1}'`
 			sed -ci -E "s/.*$i/$upd_addr\t$i/g" /etc/hosts
 		fi
 	fi
--- s_scripts/name-res.sh
+++ s_scripts/name-res.sh
@@ -56,7 +56,7 @@ sh /root/s_scripts/host-config.sh
 
 touch ${DOCKER_DIR}hosts/hosts
 
-(crontab -l 2>/dev/null; echo "*/2 * * * * ${DOCKER_DIR}s_scripts/host-config.sh 2>&1 > ${DOCKER_DIR}s_scripts/hosts.log") | crontab -
+(crontab -l 2>/dev/null; echo "*/2 * * * * ${DOCKER_DIR}s_scripts/host-config.sh &> ${DOCKER_DIR}s_scripts/hosts.log") | crontab -
 
 # NameNode
 if [ ! -f /root/namenode/current ]; then
