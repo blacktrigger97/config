6dce967d73c1c81acc206204e3d84732f8e44665
blacktrigger97 <blacktrigger97@gmail.com>
2024-04-10 09:27:24 +0530

    jupyter script added

---
+++ s_scripts/jupyter.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+#
+#
+source ~/.bashrc
+
+pyspark &
+
+# Wait for any process to exit
+wait -n
+
+# Exit with status of process that exited first
+exit $?
+
+
+#---------------------------------------------------------
+# eof
