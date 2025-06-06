fe370d7d8e60727a5c23975db75fef81635fedef
blacktrigger97 <blacktrigger97@gmail.com>
2025-06-06 17:05:59 +0530

    upd

--- s_scripts/air-flow.sh
+++ s_scripts/air-flow.sh
@@ -15,10 +15,10 @@ airflow api-server -l=/root/logs/airflow/api-server/api-server.log --stderr=/roo
 airflow scheduler -l=/root/logs/airflow/scheduler/scheduler.log --stderr=/root/logs/airflow/scheduler/scheduler.err --stdout=/root/logs/airflow/scheduler/scheduler.out --pid=/run/airflow/airflow-scheduler.pid -D
 
 # Airflow Triggerer
-airflow triggerer -l=/root/logs/airflow/triggerer/triggerer.log --stderr=/root/logs/airflow/triggerer/triggerer.err --stdout=/root/logs/airflow/triggerer/triggerer.out --pid=/run/airflow/airflow-triggerer.pid -D
+airflow dag-processor -l=/root/logs/airflow/dag_processor/dag_processor.log --stderr=/root/logs/airflow/dag_processor/dag_processor.err --stdout=/root/logs/airflow/dag_processor/dag_processor.out --pid=/run/airflow/airflow-dagprocessor.pid -D
 
 # Airflow Triggerer
-airflow dag-processor -l=/root/logs/airflow/dag_processor_manager/dag_processor_manager.log --stderr=/root/logs/airflow/dag_processor_manager/dag_processor_manager.err --stdout=/root/logs/airflow/dag_processor_manager/dag_processor_manager.out --pid=/run/airflow/airflow-dagprocessor.pid -D
+airflow triggerer -l=/root/logs/airflow/triggerer/triggerer.log --stderr=/root/logs/airflow/triggerer/triggerer.err --stdout=/root/logs/airflow/triggerer/triggerer.out --pid=/run/airflow/airflow-triggerer.pid &
 
 # Airflow Flower
 # celery flower --address='airflow.bdc.home' -l=/root/logs/airflow/flower/flower.log --stderr=/root/logs/airflow/flower/flower.err --stdout=/root/logs/airflow/flower/flower.out --pid=/run/airflow/airflow-flower.pid &
