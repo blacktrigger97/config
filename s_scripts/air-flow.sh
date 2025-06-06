aa9187d80424d30f51830aedd51ad1bdec677caf
blacktrigger97 <blacktrigger97@gmail.com>
2025-06-06 17:22:58 +0530

    upd

--- s_scripts/air-flow.sh
+++ s_scripts/air-flow.sh
@@ -21,7 +21,7 @@ airflow dag-processor -l=/root/logs/airflow/dag_processor/dag_processor.log --st
 airflow triggerer -l=/root/logs/airflow/triggerer/triggerer.log --stderr=/root/logs/airflow/triggerer/triggerer.err --stdout=/root/logs/airflow/triggerer/triggerer.out --pid=/run/airflow/airflow-triggerer.pid &
 
 # Airflow Flower
-# celery flower --address='airflow.bdc.home' -l=/root/logs/airflow/flower/flower.log --stderr=/root/logs/airflow/flower/flower.err --stdout=/root/logs/airflow/flower/flower.out --pid=/run/airflow/airflow-flower.pid &
+airflow celery flower --hostname='airflow.bdc.home' -l=/root/logs/airflow/flower/flower.log --stderr=/root/logs/airflow/flower/flower.err --stdout=/root/logs/airflow/flower/flower.out --pid=/run/airflow/airflow-flower.pid &
 
 # Wait for any process to exit
 wait -n
