0d9e52ff36b2d71adfa7509f498dee944e85c222
blacktrigger97 <blacktrigger97@gmail.com>
2025-06-06 16:54:23 +0530

    upd

--- s_scripts/air-flow.sh
+++ s_scripts/air-flow.sh
@@ -9,13 +9,16 @@ airflow db migrate
 # airflow users create -e blacktrigger97@gmail.com -f Airflow -l Admin -u admin -p admin_123 -r Admin
 
 # Airflow Webserver
-airflow api-server -l=/root/logs/airflow/webserver/webserver.log --stderr=/root/logs/airflow/webserver/webserver.err --stdout=/root/logs/airflow/webserver/webserver.out --pid=/run/airflow/airflow-webserver.pid &
+airflow api-server -l=/root/logs/airflow/api-server/api-server.log --stderr=/root/logs/airflow/api-server/api-server.err --stdout=/root/logs/airflow/api-server/api-server.out --pid=/run/airflow/airflow-server.pid &
 
 # Airflow Scheduler
-airflow scheduler -l=/root/logs/airflow/scheduler/scheduler.log --stderr=/root/logs/airflow/scheduler/scheduler.err --stdout=/root/logs/airflow/scheduler/scheduler.out --pid=/run/airflow/airflow-scheduler.pid &
+airflow scheduler -l=/root/logs/airflow/scheduler/scheduler.log --stderr=/root/logs/airflow/scheduler/scheduler.err --stdout=/root/logs/airflow/scheduler/scheduler.out --pid=/run/airflow/airflow-scheduler.pid -D
 
 # Airflow Triggerer
-airflow triggerer -l=/root/logs/airflow/triggerer/triggerer.log --stderr=/root/logs/airflow/triggerer/triggerer.err --stdout=/root/logs/airflow/triggerer/triggerer.out --pid=/run/airflow/airflow-triggerer.pid &
+airflow triggerer -l=/root/logs/airflow/triggerer/triggerer.log --stderr=/root/logs/airflow/triggerer/triggerer.err --stdout=/root/logs/airflow/triggerer/triggerer.out --pid=/run/airflow/airflow-triggerer.pid -D
+
+# Airflow Triggerer
+airflow dag-processor -l=/root/logs/airflow/dag_processor_manager/dag_processor_manager.log --stderr=/root/logs/airflow/dag_processor_manager/dag_processor_manager.err --stdout=/root/logs/airflow/dag_processor_manager/dag_processor_manager.out --pid=/run/airflow/airflow-dagprocessor.pid -D
 
 # Airflow Flower
 # celery flower --address='airflow.bdc.home' -l=/root/logs/airflow/flower/flower.log --stderr=/root/logs/airflow/flower/flower.err --stdout=/root/logs/airflow/flower/flower.out --pid=/run/airflow/airflow-flower.pid &
