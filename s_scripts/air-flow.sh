a34bb817d8b3f59e3a4cc08af8547889ccfa0dc0
blacktrigger97 <blacktrigger97@gmail.com>
2025-06-06 16:29:45 +0530

    upd

--- s_scripts/air-flow.sh
+++ s_scripts/air-flow.sh
@@ -5,8 +5,6 @@ source ~/.profile
 # Airflow Initialize Database
 airflow db migrate 
 
-sleep 1200
-
 # Airflow Admin Creation
 # airflow users create -e blacktrigger97@gmail.com -f Airflow -l Admin -u admin -p admin_123 -r Admin
 
