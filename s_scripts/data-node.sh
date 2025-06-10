dac0c90203b93684fe8ca04828d115ed8634d8a0
blacktrigger97 <blacktrigger97@gmail.com>
2025-06-10 19:44:07 +0530

    upd

--- s_scripts/data-node.sh
+++ s_scripts/data-node.sh
@@ -40,7 +40,7 @@ ssh-keygen -t rsa -f /etc/ssh/ssh_host_ed25519_key -N ''
 source ~/.profile
 
 # uv package download
-uv pip sync --system /root/airflow/pyproject.toml
+uv pip install --system /root/airflow/pyproject.toml
 
 # Airflow Worker
 airflow celery worker -H `hostname` -l=/root/airflow/logs/worker/worker.log --stderr=/root/airflow/logs/worker/worker.err --stdout=/root/airflow/logs/worker/worker.out --pid=/run/airflow/airflow-worker.pid &
