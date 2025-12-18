c443cb1f5b8e55586d9d886d31ae01846f3c07b4
blacktrigger97 <blacktrigger97@gmail.com>
2025-12-18 12:27:04 +0530

    upd 2

--- s_scripts/airflow-m.sh
+++ s_scripts/airflow-m.sh
@@ -10,18 +10,18 @@ chmod 777 /opt/airflow/password.json
 
 # sleep 600
 # Airflow Initialize Database
-airflow db migrate 
+airflow db migrate
 
 # Airflow Admin Creation
 # airflow users create -e blacktrigger97@gmail.com -f Airflow -l Admin -u admin -p admin_123 -r Admin
 
-# Airflow Webserver
+# Airflow Api Server
 airflow api-server -l=/opt/airflow/logs/api-server/api-server.log --stderr=/opt/airflow/logs/api-server/api-server.err --stdout=/opt/airflow/logs/api-server/api-server.out --pid=/run/airflow/airflow-server.pid &
 
 # Airflow Scheduler
 airflow scheduler -l=/opt/airflow/logs/scheduler/scheduler.log --stderr=/opt/airflow/logs/scheduler/scheduler.err --stdout=/opt/airflow/logs/scheduler/scheduler.out --pid=/run/airflow/airflow-scheduler.pid &
 
-# Airflow Triggerer
+# Airflow Dag Processor
 airflow dag-processor -l=/opt/airflow/logs/dag_processor/dag_processor.log --stderr=/opt/airflow/logs/dag_processor/dag_processor.err --stdout=/opt/airflow/logs/dag_processor/dag_processor.out --pid=/run/airflow/airflow-dagprocessor.pid &
 
 # Airflow Triggerer
