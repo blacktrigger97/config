24ea9b0c897d7a63c82c48a56546a918373539a5
blacktrigger97 <blacktrigger97@gmail.com>
2025-12-22 16:19:53 +0530

    upd 2

--- s_scripts/airflow-m.sh
+++ s_scripts/airflow-m.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# source ~/.profile
+source ~/.profile
 
 # uv package download
 # cd /opt/airflow
@@ -16,7 +16,7 @@ airflow db migrate
 # airflow users create -e blacktrigger97@gmail.com -f Airflow -l Admin -u admin -p admin_123 -r Admin
 
 # Airflow Api Server
-airflow api-server -l=/opt/airflow/logs/api-server/api-server.log --stderr=/opt/airflow/logs/api-server/api-server.err --stdout=/opt/airflow/logs/api-server/api-server.out --pid=/run/airflow/airflow-server.pid &
+airflow api-server -H `hostname` -l=/opt/airflow/logs/api-server/api-server.log --stderr=/opt/airflow/logs/api-server/api-server.err --stdout=/opt/airflow/logs/api-server/api-server.out --pid=/run/airflow/airflow-server.pid &
 
 # Airflow Scheduler
 airflow scheduler -l=/opt/airflow/logs/scheduler/scheduler.log --stderr=/opt/airflow/logs/scheduler/scheduler.err --stdout=/opt/airflow/logs/scheduler/scheduler.out --pid=/run/airflow/airflow-scheduler.pid &
@@ -25,10 +25,10 @@ airflow scheduler -l=/opt/airflow/logs/scheduler/scheduler.log --stderr=/opt/air
 airflow dag-processor -l=/opt/airflow/logs/dag_processor/dag_processor.log --stderr=/opt/airflow/logs/dag_processor/dag_processor.err --stdout=/opt/airflow/logs/dag_processor/dag_processor.out --pid=/run/airflow/airflow-dagprocessor.pid &
 
 # Airflow Triggerer
-airflow triggerer -l=/opt/airflow/logs/triggerer/triggerer.log --stderr=/opt/airflow/logs/triggerer/triggerer.err --stdout=/opt/airflow/logs/triggerer/triggerer.out --pid=/run/airflow/airflow-triggerer.pid &
+airflow triggerer -H `hostname` -l=/opt/airflow/logs/triggerer/triggerer.log --stderr=/opt/airflow/logs/triggerer/triggerer.err --stdout=/opt/airflow/logs/triggerer/triggerer.out --pid=/run/airflow/airflow-triggerer.pid &
 
 # Airflow Flower
-airflow celery flower --hostname='airflow.bdc.home' -l=/opt/airflow/logs/flower/flower.log --stderr=/opt/airflow/logs/flower/flower.err --stdout=/opt/airflow/logs/flower/flower.out --pid=/run/airflow/airflow-flower.pid &
+airflow celery flower --hostname `hostname` -l=/opt/airflow/logs/flower/flower.log --stderr=/opt/airflow/logs/flower/flower.err --stdout=/opt/airflow/logs/flower/flower.out --pid=/run/airflow/airflow-flower.pid &
 
 # Wait for any process to exit
 wait -n
--- s_scripts/airflow-w.sh
+++ s_scripts/airflow-w.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 
-# source ~/.profile
+source ~/.profile
 
 # uv package download
 # cd /opt/airflow
 # uv pip install --system -r pyproject.toml
 
-host=`hostname`
 # Airflow Worker
-airflow celery worker -H $host -l=/opt/airflow/logs/worker/worker.log --stderr=/opt/airflow/logs/worker/worker.err --stdout=/opt/airflow/logs/worker/worker.out --pid=/run/airflow/airflow-worker.pid &
+airflow celery worker -H `hostname` -l=/opt/airflow/logs/worker/worker.log --stderr=/opt/airflow/logs/worker/worker.err --stdout=/opt/airflow/logs/worker/worker.out --pid=/run/airflow/airflow-worker.pid &
 
 # Wait for any process to exit
 wait -n
