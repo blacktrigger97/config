e0f58cde3f9361b4509020f1e32bdba56599e3d5
blacktrigger97 <blacktrigger97@gmail.com>
2025-12-22 16:30:36 +0530

    upd 2

--- s_scripts/airflow-m.sh
+++ s_scripts/airflow-m.sh
@@ -16,19 +16,19 @@ airflow db migrate
 # airflow users create -e blacktrigger97@gmail.com -f Airflow -l Admin -u admin -p admin_123 -r Admin
 
 # Airflow Api Server
-airflow api-server -H `hostname` -l=/opt/airflow/logs/api-server/api-server.log --stderr=/opt/airflow/logs/api-server/api-server.err --stdout=/opt/airflow/logs/api-server/api-server.out --pid=/run/airflow/airflow-server.pid &
+airflow api-server -H `hostname` # -l=/opt/airflow/logs/api-server/api-server.log --stderr=/opt/airflow/logs/api-server/api-server.err --stdout=/opt/airflow/logs/api-server/api-server.out --pid=/run/airflow/airflow-server.pid &
 
 # Airflow Scheduler
-airflow scheduler -l=/opt/airflow/logs/scheduler/scheduler.log --stderr=/opt/airflow/logs/scheduler/scheduler.err --stdout=/opt/airflow/logs/scheduler/scheduler.out --pid=/run/airflow/airflow-scheduler.pid &
+airflow scheduler # -l=/opt/airflow/logs/scheduler/scheduler.log --stderr=/opt/airflow/logs/scheduler/scheduler.err --stdout=/opt/airflow/logs/scheduler/scheduler.out --pid=/run/airflow/airflow-scheduler.pid &
 
 # Airflow Dag Processor
-airflow dag-processor -l=/opt/airflow/logs/dag_processor/dag_processor.log --stderr=/opt/airflow/logs/dag_processor/dag_processor.err --stdout=/opt/airflow/logs/dag_processor/dag_processor.out --pid=/run/airflow/airflow-dagprocessor.pid &
+airflow dag-processor # -l=/opt/airflow/logs/dag_processor/dag_processor.log --stderr=/opt/airflow/logs/dag_processor/dag_processor.err --stdout=/opt/airflow/logs/dag_processor/dag_processor.out --pid=/run/airflow/airflow-dagprocessor.pid &
 
 # Airflow Triggerer
-airflow triggerer -H `hostname` -l=/opt/airflow/logs/triggerer/triggerer.log --stderr=/opt/airflow/logs/triggerer/triggerer.err --stdout=/opt/airflow/logs/triggerer/triggerer.out --pid=/run/airflow/airflow-triggerer.pid &
+airflow triggerer -H `hostname` # -l=/opt/airflow/logs/triggerer/triggerer.log --stderr=/opt/airflow/logs/triggerer/triggerer.err --stdout=/opt/airflow/logs/triggerer/triggerer.out --pid=/run/airflow/airflow-triggerer.pid &
 
 # Airflow Flower
-airflow celery flower --hostname `hostname` -l=/opt/airflow/logs/flower/flower.log --stderr=/opt/airflow/logs/flower/flower.err --stdout=/opt/airflow/logs/flower/flower.out --pid=/run/airflow/airflow-flower.pid &
+airflow celery flower --hostname `hostname` # -l=/opt/airflow/logs/flower/flower.log --stderr=/opt/airflow/logs/flower/flower.err --stdout=/opt/airflow/logs/flower/flower.out --pid=/run/airflow/airflow-flower.pid &
 
 # Wait for any process to exit
 wait -n
