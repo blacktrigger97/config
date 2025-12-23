d99dd5581fe45e3ca0a05275c5f2030c20befc86
blacktrigger97 <blacktrigger97@gmail.com>
2025-12-23 20:36:45 +0530

    upd 2

--- s_scripts/airflow-w.sh
+++ s_scripts/airflow-w.sh
@@ -9,7 +9,7 @@ source ~/.profile
 # Airflow Worker
 host=`hostname`
 echo "hostname: $host"
-airflow celery worker -H $host & # -l=/opt/airflow/logs/worker/worker.log --stderr=/opt/airflow/logs/worker/worker.err --stdout=/opt/airflow/logs/worker/worker.out --pid=/run/airflow/airflow-worker.pid &
+airflow celery worker -H `hostname` & # -l=/opt/airflow/logs/worker/worker.log --stderr=/opt/airflow/logs/worker/worker.err --stdout=/opt/airflow/logs/worker/worker.out --pid=/run/airflow/airflow-worker.pid &
 
 # Wait for any process to exit
 wait -n
