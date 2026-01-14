b8b46b98b0df29e181841d7bfd8a3d32c24da6ef
blacktrigger97 <blacktrigger97@gmail.com>
2026-01-14 13:04:45 +0530

    upd

--- s_scripts/airflow-w.sh
+++ s_scripts/airflow-w.sh
@@ -7,7 +7,7 @@ source ~/.profile
 # uv pip install --system -r pyproject.toml
 
 # Airflow Worker
-airflow celery worker -H `hostname` --without-mingle -l=/root/airflow/logs/worker/worker.log --stderr=/root/airflow/logs/worker/worker.err --stdout=/root/airflow/logs/worker/worker.out --pid=/run/airflow/airflow-worker.pid &
+airflow celery worker -H `hostname` -l=/root/airflow/logs/worker/worker.log --stderr=/root/airflow/logs/worker/worker.err --stdout=/root/airflow/logs/worker/worker.out --pid=/run/airflow/airflow-worker.pid &
 
 # Wait for any process to exit
 wait -n
